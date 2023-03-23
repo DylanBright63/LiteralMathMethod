5ec8ca631609bd31980981df0eeb360a0d036ea2
Student <Brightdylan93@gmail.com>
2023-03-23 08:43:52 -0500

    done

---
+++ script.js
@@ -0,0 +1,26 @@
+let warmHugs = "hi, I'm Olaf and I like Warm Hugs."
+
+
+console.log(warmHugs.toUpperCase)
+
+
+warmhugs = warmHugs.replace('like', 'love');
+console.log(warmHugs);
+
+
+let beenimpaled = 'oh, look at that. i/'ve been impaled!';
+
+
+console.log (beenimpaled.slice(-18));
+
+
+let dotDotDot = "...";
+
+
+let skullbones = '"i don't have a skull ${dotDotDot}or bones?";
+console.log(skullbones)
+
+
+console.log(Math.PI);
+
+let ranmdom = math.floor(math.random() * 3)+ 1;
